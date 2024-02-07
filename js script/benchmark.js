4999068bee42c270f0ba83222cb3487c4aad69db
MARCO DELPOPOLO <156794727+MarcoDelpimme@users.noreply.github.com>
2024-02-07 17:54:14 +0100

    FunctionJsFinish

--- js script/benchmark.js
+++ js script/benchmark.js
@@ -81,6 +81,7 @@ const questions = [
     incorrect_answers: ["Python", "C", "Jakarta"],
   },
 ];
+
 const COLOR_CODES = {
   info: {
     color: "green",
@@ -105,23 +106,8 @@ let timePassed = 0; //timer
 let timeLeft = THE_LIMIT; // Tempo iniziale
 let timerInterval = null; //timer
 let remainingPathColor = COLOR_CODES.info.color; //timer
+let currentSelectedAnswer = null;
 
-//reset del background quando cambi risposta
-function cleanse() {
-  const buttons = document.querySelectorAll(".inputpg2");
-  buttons.forEach((button) => {
-    button.style.backgroundColor = "";
-  });
-}
-//randomizza ordine risposte
-function shuffleQuestion(array) {
-  for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
-  }
-}
-
-// funzione generale per creazione dei div con button interni
 function questionStep() {
   const obj = questions[indexQuestion];
   container.innerHTML = "";
@@ -132,11 +118,11 @@ function questionStep() {
 
   const form = document.createElement("form");
   form.classList.add("formpg2");
-  // concatenazione di risposte correte e incorrette
   const options = obj.incorrect_answers.concat(obj.correct_answer);
   shuffleQuestion(options);
   const divAll = document.createElement("div");
   divAll.classList.add("divAll");
+
   options.forEach((option) => {
     const div = document.createElement("div");
     div.classList.add("divInputPg2");
@@ -153,28 +139,29 @@ function questionStep() {
     divAll.appendChild(div);
   });
 }
-//funzione per selezione della risposta
-const wrong = [];
-const correct = [];
-function selectAnswer(option, button) {
-  cleanse();
-  button.style.backgroundColor = "#c2128d";
-  dataAnswer(option);
+
+function shuffleQuestion(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
 }
 
-//funzione prossima domanda che scala con indice -- inserito parametri timer che si resettano tramite valore booleano
 function nextQuestion() {
+  let selectedAnswer = null;
   indexQuestion++;
   quizCompleted = false;
   if (indexQuestion < questions.length) {
+    dataAnswer(selectedAnswer);
+    selectedAnswer = null;
     questionStep();
     clearInterval(timerInterval);
-    timePassed = 0; // Reimposta il tempo trascorso
-    timeLeft = THE_LIMIT; // Reimposta il tempo rimasto
+    timePassed = 0;
+    timeLeft = THE_LIMIT;
     document.getElementById("base-timer-label").innerHTML = formatTime(timeLeft);
     setCircleDasharray();
     setRemainingPathColor(timeLeft);
-    startTimer(); // Avvia il nuovo timer
+    startTimer();
   } else {
     const lastPage = document.getElementById("lastpage");
     container.style.display = "none";
@@ -182,120 +169,140 @@ function nextQuestion() {
     timer.style.display = "none";
     const buttonNext = document.getElementById("next-question-btn");
     buttonNext.style.display = "none";
-    lastPage.innerHTML = `<canvas id="myChart" width="400" height="400"></canvas>`;
+    lastPage.innerHTML = `<canvas id="chartId" aria-label="chart" height="350" width="580"></canvas>`;
     clearInterval(timerInterval);
     generatePieChart();
+    wrong.length = 0;
+    correct.length = 0;
+  }
+  if (!quizCompleted) {
+    dataAnswer(null);
   }
 }
 
-//timer interno funzione
-
-function formatTime(time) {
-  const minutes = Math.floor(time / 60);
-  let seconds = time % 60;
-
-  if (seconds < 10) {
-    seconds = `0${seconds}`;
-  }
-
-  return `${minutes}:${seconds}`;
-}
-
-//bottone per cambio di domanda
 const nextQuestionBtn = document.getElementById("next-question-btn");
 nextQuestionBtn.addEventListener("click", function () {
   dataAnswer(null);
   nextQuestion();
 });
 
-const FULL_DASH_ARRAY = 283;
-const WARNING_THRESHOLD = 15;
-const ALERT_THRESHOLD = 5;
+const wrong = [];
+const correct = [];
 
-//variazione del colore in base al tempo
-function setRemainingPathColor(timeLeft) {
-  const { alert, warning, info } = COLOR_CODES;
-  const pathElement = document.getElementById("base-timer-path-remaining");
-
-  pathElement.classList.remove(alert.color, warning.color, info.color);
-
-  if (timeLeft <= alert.threshold) {
-    pathElement.classList.add(alert.color);
-  } else if (timeLeft <= warning.threshold) {
-    pathElement.classList.add(warning.color);
-  } else {
-    pathElement.classList.add(info.color);
+function selectAnswer(option, button) {
+  if (quizCompleted || timeLeft === 0) {
+    return;
   }
-}
 
-//calcolo frazioni di tempo
-function calculateTimeFraction() {
-  const rawTimeFraction = timeLeft / TIME_LIMIT;
-  return rawTimeFraction - (1 / THE_LIMIT) * (1 - rawTimeFraction);
-}
+  if (currentSelectedAnswer !== null) {
+    const prevSelectedButton = Array.from(document.querySelectorAll(".inputpg2")).find(
+      (btn) => btn.innerText === currentSelectedAnswer
+    );
 
-//array 60 secondi
-function setCircleDasharray() {
-  const circleDasharray = `${(calculateTimeFraction() * FULL_DASH_ARRAY).toFixed(0)} 283`;
-  document.getElementById("base-timer-path-remaining").setAttribute("stroke-dasharray", circleDasharray);
-}
-
-// timer start
-function startTimer() {
-  timerInterval = setInterval(() => {
-    timePassed = timePassed += 1;
-    timeLeft = THE_LIMIT - timePassed;
-    document.getElementById("base-timer-label").innerHTML = formatTime(timeLeft);
-    setCircleDasharray();
-    setRemainingPathColor(timeLeft);
-
-    if (timeLeft === 0) {
-      onTimesUp();
+    if (prevSelectedButton) {
+      prevSelectedButton.style.backgroundColor = "";
     }
-  }, 1000);
+  }
+
+  button.style.backgroundColor = "#c2128d";
+  currentSelectedAnswer = option;
 }
 
-function dataAnswer(selectedQuestion) {
+function cleanse() {
+  const buttons = document.querySelectorAll(".inputpg2");
+  buttons.forEach((button) => {
+    button.style.backgroundColor = "";
+  });
+}
+
+function dataAnswer() {
+  console.log("selected question", currentSelectedAnswer);
   const currentQuestion = questions[indexQuestion];
-  console.log(currentQuestion);
-  console.log(selectedQuestion);
 
   const answerData = {
     question: currentQuestion.question,
-    selectedQuestion: selectedQuestion,
+    selectedQuestion: currentSelectedAnswer,
     correct_answer: currentQuestion.correct_answer,
   };
-  if (selectedQuestion === currentQuestion.correct_answer) {
+  console.log("answer data", answerData);
+
+  if (currentSelectedAnswer === currentQuestion.correct_answer) {
     correct.push(answerData);
-    console.log("TRUE");
-  } else {
+  } else if (currentSelectedAnswer !== null) {
     wrong.push(answerData);
-    console.log("FALSE");
   }
+  console.log("correct", correct);
+  console.log("wrong", wrong);
 }
 
 function onTimesUp() {
   clearInterval(timerInterval);
-  dataAnswer(null);
+  quizCompleted = true;
   nextQuestion();
+  dataAnswer(null);
 }
 
 function generatePieChart() {
   const totalQuestions = questions.length;
+  const answeredQuestions = correct.length + wrong.length;
+  const unansweredQuestions = totalQuestions - answeredQuestions;
+
   const correctPercentage = (correct.length / totalQuestions) * 100;
   const wrongPercentage = (wrong.length / totalQuestions) * 100;
 
-  const ctx = document.getElementById("myChart").getContext("2d");
-  const myChart = new Chart(ctx, {
-    type: "pie",
+  const realPercentageWrong = (unansweredQuestions / totalQuestions) * 100 + (wrong.length / totalQuestions) * 100;
+  if (correctPercentage <= 60) {
+    const containerResult = document.getElementById("result-message");
+    const p = document.createElement("p");
+
+    p.innerText = "Hai fallito il test";
+    p.style.color = "red";
+
+    containerResult.appendChild(p);
+  } else {
+    const containerResult = document.getElementById("result-message");
+    const p = document.createElement("p");
+
+    p.innerText = "Hai superato il test";
+    p.style.color = "green";
+
+    containerResult.appendChild(p);
+  }
+  const percentualecorrette = document.getElementById("percentualecorrette");
+  percentualecorrette.innerHTML = "CORRETTE" + " " + correctPercentage + "%";
+  percentualecorrette.style.color = "white";
+  percentualecorrette.classList.add("correctpercent");
+
+  const percentualescorrette = document.getElementById("percentualescorrette");
+  percentualescorrette.innerHTML = "SBAGLIATE" + " " + realPercentageWrong + "%";
+  percentualescorrette.style.color = "white";
+  percentualescorrette.classList.add("wrongpercent");
+
+  const buttonpage2 = document.getElementById("container-btn2");
+  const buttonInput = document.createElement("button");
+  buttonInput.innerText = "RATE US";
+  buttonInput.value = "a";
+  buttonInput.classList.add("btnpage2");
+  const anchor = document.createElement("a");
+  anchor.href = "../feedback.html";
+  anchor.appendChild(buttonInput);
+  buttonpage2.appendChild(anchor);
+
+  var chrt = document.getElementById("chartId").getContext("2d");
+  var chartId = new Chart(chrt, {
+    type: "doughnut",
     data: {
-      labels: ["Corrette", "Sbagliate"],
+      labels: ["SBAGLIATE", "CORRETTE"],
       datasets: [
         {
-          data: [correctPercentage, wrongPercentage],
-          backgroundColor: ["green", "red"],
+          data: [realPercentageWrong, correctPercentage],
+          backgroundColor: ["red", "green"],
+          hoverOffset: 5,
         },
       ],
+    },
+    options: {
+      responsive: false,
     },
   });
 }
@@ -321,6 +328,60 @@ document.getElementById("app").innerHTML = `
       <span id="base-timer-label" class="base-timer__label">${formatTime(timeLeft)}</span>
     </div>
     `;
+
+function formatTime(time) {
+  const minutes = Math.floor(time / 60);
+  let seconds = time % 60;
+
+  if (seconds < 10) {
+    seconds = `0${seconds}`;
+  }
+
+  return `${minutes}:${seconds}`;
+}
+
+const FULL_DASH_ARRAY = 283;
+const WARNING_THRESHOLD = 15;
+const ALERT_THRESHOLD = 5;
+
+function setRemainingPathColor(timeLeft) {
+  const { alert, warning, info } = COLOR_CODES;
+  const pathElement = document.getElementById("base-timer-path-remaining");
+
+  pathElement.classList.remove(alert.color, warning.color, info.color);
+
+  if (timeLeft <= alert.threshold) {
+    pathElement.classList.add(alert.color);
+  } else if (timeLeft <= warning.threshold) {
+    pathElement.classList.add(warning.color);
+  } else {
+    pathElement.classList.add(info.color);
+  }
+}
+
+function calculateTimeFraction() {
+  const rawTimeFraction = timeLeft / TIME_LIMIT;
+  return rawTimeFraction - (1 / THE_LIMIT) * (1 - rawTimeFraction);
+}
+
+function setCircleDasharray() {
+  const circleDasharray = `${(calculateTimeFraction() * FULL_DASH_ARRAY).toFixed(0)} 283`;
+  document.getElementById("base-timer-path-remaining").setAttribute("stroke-dasharray", circleDasharray);
+}
+
+function startTimer() {
+  timerInterval = setInterval(() => {
+    timePassed = timePassed += 1;
+    timeLeft = THE_LIMIT - timePassed;
+    document.getElementById("base-timer-label").innerHTML = formatTime(timeLeft);
+    setCircleDasharray();
+    setRemainingPathColor(timeLeft);
+
+    if (timeLeft === 0) {
+      onTimesUp();
+    }
+  }, 1000);
+}
 
 startTimer();
 questionStep();
